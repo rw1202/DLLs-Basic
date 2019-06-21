630413121ac47b8ec0a52b9c02b3d111ed96fe11
rw1202 <44084938+rw1202@users.noreply.github.com>
2019-06-21 15:30:05 +0100

    Add files via upload
    Complete

---
+++ ProductManager.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Collections.ObjectModel
+Imports System.Configuration
+Imports AdventureWorks.EntityLayer
+Imports Common.Library
+
+
+Public Class ProductManager
+    Inherits DataManagerBase
+
+    Private ProductID As Integer
+
+    Function LoadProducts() As ObservableCollection(Of Product)
+        Return LoadProducts()
+    End Function
+
+    Function LoadProducts(ByVal startingFilePath As String) As ObservableCollection(Of Product)
+        Dim ret = New ObservableCollection(Of Product)()
+
+        Try
+            Dim doc = MyBase.LoadXElement(
+                ConfigurationManager.AppSettings("Products File"),
+                startingFilePath)
+            Dim products = From prod In doc.<Product>
+                           Select New ProductManager With {
+                           .ProductID = Convert.ToInt32(prod.Element("ProductID"))
+                           }
+        Catch ex As Exception
+
+        End Try
+    End Function
+End Class
